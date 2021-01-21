8caee333c24cee4c93d53f34d1443a59c133b2ea
Tony Salter <tonysalter@Tonys-MacBook-Pro-133.local>
2021-01-21 11:34:45 +0000

    Updates

---
+++ precache-manifest.8f58e55de6a786fe3806d10f102e2094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81fc1c27dcba4db3f19ba45b35df8ea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b0a7e77446ba2de13d9",
+    "url": "/monsters-rolodex/static/css/main.636f8633.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0a7e77446ba2de13d9",
+    "url": "/monsters-rolodex/static/js/main.49d57e2c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
